80fee8c2a98c952430d28625b68efff49d35cab2
drili <DriliB@hotmail.com>
2022-12-29 16:42:02 +0100

    :recycle: S1-S9, re-uploaded

--- jobify/client/src/App.js
+++ jobify/client/src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Dashboard, Register, Landing, Error } from "./pages"
 
-
 function App() {
     return (
         <BrowserRouter>
