c3627552a50df3254695d38c0667bd7feb18cb76
drili <DriliB@hotmail.com>
2022-12-30 02:39:40 +0100

    :hammer: S12 - Register Controller Initial Setup (Register controller, error to middleware, express-async-errors, status-codes)

--- jobify/controllers/authController.js
+++ jobify/controllers/authController.js
@@ -1,5 +1,19 @@
+import User from "../models/User.js"
+import { StatusCodes } from "http-status-codes"
+
 const register = async (req, res) => {
-    res.send("register user")
+    // res.send("register user")
+
+    // try {
+    //     const user = await User.create(req.body)
+    //     res.status(201).json({ user })
+    // } catch (error) {
+    //     // res.status(500).json({ msg: "There was an error registering user." })
+    //     next(error)
+    // }
+
+    const user = await User.create(req.body)
+    res.status(StatusCodes.CREATED).json({ user })
 }
 
 const login = async (req, res) => {
